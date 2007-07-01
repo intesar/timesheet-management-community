cb748c2c54bca200e9f4ca12cbbd13e231b41458
mohdhamedmscse <mohdhamedmscse@20ed3424-da33-0410-92c3-5597d9071429>
2007-07-01 23:35:26 +0000

    Added DAO and BO

---
+++ trunk/authenticator/src/java/com/abbh/authenticator/bo/UserBO.java
@@ -0,0 +1,24 @@
+/*
+ * UserBO.java
+ * 
+ * Created on Jul 1, 2007, 4:27:41 PM
+ * 
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.abbh.authenticator.bo;
+
+import com.abbh.authenticator.entity.Users;
+
+/**
+ *
+ * @author Mohammed Hamed
+ */
+public interface UserBO {
+    
+    public void  addUser(Users user);
+ 
+    
+
+}
---
+++ trunk/authenticator/src/java/com/abbh/authenticator/bo/impl/UserBOImpl.java
@@ -0,0 +1,28 @@
+/*
+ * UserBOImpl.java
+ * 
+ * Created on Jul 1, 2007, 4:27:55 PM
+ * 
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.abbh.authenticator.bo.impl;
+
+import com.abbh.authenticator.bo.UserBO;
+import com.abbh.authenticator.entity.Users;
+
+/**
+ *
+ * @author Mohammed Hamed
+ */
+public class UserBOImpl implements UserBO {
+
+    public UserBOImpl() {
+    }
+
+    public void addUser(Users user) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
