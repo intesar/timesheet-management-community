95c96e4d1a63cab19b5793ad7231e17ae3f14207
mohdhamedmscse <mohdhamedmscse@20ed3424-da33-0410-92c3-5597d9071429>
2007-07-01 23:35:26 +0000

    Added DAO and BO

---
+++ authenticator/src/java/com/abbh/authenticator/dao/UserDAO.java
@@ -0,0 +1,23 @@
+/*
+ * UserDAO.java
+ * 
+ * Created on Jul 1, 2007, 4:18:52 PM
+ * 
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.abbh.authenticator.dao;
+
+import com.abbh.authenticator.entity.Users;
+
+/**
+ *
+ * @author Mohammed Hamed
+ */
+public interface UserDAO {
+
+    
+    
+    public void save(Users user);
+}
---
+++ authenticator/src/java/com/abbh/authenticator/dao/impl/UserDAOImpl.java
@@ -0,0 +1,62 @@
+/*
+ * UserDAOImpl.java
+ * 
+ * Created on Jul 1, 2007, 4:19:35 PM
+ * 
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.abbh.authenticator.dao.impl;
+
+import com.abbh.authenticator.dao.UserDAO;
+import com.abbh.authenticator.entity.Users;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Mohammed Hamed
+ */
+public class UserDAOImpl implements UserDAO{
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("SecureAuthPU");
+    
+    
+
+    public UserDAOImpl() {
+    }
+
+    public void save(Users user) {
+          EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(user);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            java.util.logging.Logger.getLogger(getClass().getName()).log(java.util.logging.Level.SEVERE, "exception caught", e);
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+        
+      
+        
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void persist(Object object) {
+        javax.persistence.EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            java.util.logging.Logger.getLogger(getClass().getName()).log(java.util.logging.Level.SEVERE, "exception caught", e);
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+}
