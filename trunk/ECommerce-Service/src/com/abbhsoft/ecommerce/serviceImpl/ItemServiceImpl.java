ec36f1d185bd7bd0e46e331ad78f20222aa166b0
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-08-21 03:20:17 +0000

    ECommerce-Service

---
+++ trunk/ECommerce-Service/src/com/abbhsoft/ecommerce/serviceImpl/ItemServiceImpl.java
@@ -0,0 +1,32 @@
+/*
+ * ItemServiceImpl.java
+ * 
+ * Created on Aug 20, 2007, 11:13:38 PM
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.abbhsoft.ecommerce.serviceImpl;
+
+import com.abbhsoft.ecommerce.service.ItemService;
+import java.util.List;
+
+/**
+ *
+ * @author shannan
+ */
+public class ItemServiceImpl implements ItemService {
+
+    public ItemServiceImpl() {
+    }
+
+    public List getCatagories() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List getCatagoryItems(String catagory) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
