14b3a066443df149486f9c4c57f72a06365b5273
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-08-21 03:29:02 +0000

    updated with interface

--- ECommerce-Service/src/com/abbhsoft/ecommerce/serviceImpl/ItemServiceImpl.java
+++ ECommerce-Service/src/com/abbhsoft/ecommerce/serviceImpl/ItemServiceImpl.java
@@ -1,14 +1,15 @@
 /*
  * ItemServiceImpl.java
- * 
+ *
  * Created on Aug 20, 2007, 11:13:38 PM
- * 
+ *
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 
 package com.abbhsoft.ecommerce.serviceImpl;
 
+import com.abbhsoft.ecommerce.dao.ItemDAO;
 import com.abbhsoft.ecommerce.service.ItemService;
 import java.util.List;
 
@@ -29,4 +30,10 @@ public class ItemServiceImpl implements ItemService {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    public void setItemDAO(ItemDAO itemDAO) {
+        this.itemDAO = itemDAO;
+    }
+
+
+    private ItemDAO itemDAO;
 }
