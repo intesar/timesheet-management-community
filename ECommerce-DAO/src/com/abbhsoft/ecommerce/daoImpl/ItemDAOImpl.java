bba8bc096f4024a77f36ac81ecdedc5c52890346
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-08-21 03:19:28 +0000

    updated with interface

---
+++ ECommerce-DAO/src/com/abbhsoft/ecommerce/daoImpl/ItemDAOImpl.java
@@ -0,0 +1,32 @@
+/*
+ * ItemDAOImpl.java
+ * 
+ * Created on Aug 20, 2007, 11:15:30 PM
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.abbhsoft.ecommerce.daoImpl;
+
+import com.abbhsoft.ecommerce.dao.ItemDAO;
+import java.util.List;
+
+/**
+ *
+ * @author shannan
+ */
+public class ItemDAOImpl implements ItemDAO {
+
+    public ItemDAOImpl() {
+    }
+
+    public List findAllCatagories() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List findCatagoryItems(String catagory) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
