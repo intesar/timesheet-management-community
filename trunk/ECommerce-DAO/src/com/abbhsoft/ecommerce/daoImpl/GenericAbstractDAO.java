41cdb75bbf1c54c8012f8a896e068aa76cfaa5f6
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-08-22 02:48:15 +0000

    updated

--- trunk/ECommerce-DAO/src/com/abbhsoft/ecommerce/daoImpl/GenericAbstractDAO.java
+++ trunk/ECommerce-DAO/src/com/abbhsoft/ecommerce/daoImpl/GenericAbstractDAO.java
@@ -44,9 +44,13 @@ public abstract class GenericAbstractDAO<T, Pk extends Serializable> extends Jpa
         return (T) this.find(clazz, pk);
     }
 
+    private String getClassName() {
+        return clazz.getName().replace("com.abbhsoft.ecommerce.model.", "");
+    }
     @SuppressWarnings(value = "unchecked")
     public Collection<T> findAll() {
-        final String ql = " select o from " + clazz.getName() + " where o.isEnabled = true ";
+        
+        final String ql = " select o from " +  this.getClassName() + " as o where o.isEnabled = true ";
         return (Collection<T>) execute(new JpaCallback() {
             public Object doInJpa(EntityManager em) throws PersistenceException {                
                 Query query = em.createQuery(ql);                
