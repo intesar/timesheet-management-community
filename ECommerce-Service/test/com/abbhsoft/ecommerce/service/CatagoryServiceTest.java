9f8f6d66f78a87cc785bcc70c4547375f2da7526
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-08-22 02:48:22 +0000

    updated

--- ECommerce-Service/test/com/abbhsoft/ecommerce/service/CatagoryServiceTest.java
+++ ECommerce-Service/test/com/abbhsoft/ecommerce/service/CatagoryServiceTest.java
@@ -16,54 +16,75 @@ import org.springframework.test.AbstractTransactionalDataSourceSpringContextTest
  */
 public class CatagoryServiceTest extends AbstractTransactionalDataSourceSpringContextTests {
 
-  
+    private CatagoryService catagoryService;
 
-    private CatagoryService itemService;
-
-    public void setCompanyDAO(CatagoryService itemService) {
-        this.itemService = itemService;
+    public void setCatagoryService(CatagoryService catagoryService) {
+        this.catagoryService = catagoryService;
     }
+
+    
 
     @Override
     protected String[] getConfigLocations() {
-        return new String[]{"classpath:context/applicationContext-DAO.xml"};
+        return new String[]{"classpath:context/applicationContext-DAO.xml", 
+                             "classpath:context/applicationContext-Services.xml",
+                             "classpath:context/applicationContext-Transactions.xml"};
     }
 
     @Override
     protected void onSetUpInTransaction() throws Exception {
 
-//        jdbcTemplate.execute("insert into users ( id, email ) values ( 1, 'test')");
-//        jdbcTemplate.execute("insert into company (id, name, type, city, owner ) values ( 1, 'name1', 'type1', 'chicago', 'test')");
-            }
+        jdbcTemplate.execute("insert into catagory (id, name, is_enabled ) values ( 1, 'DINNER', TRUE)");
+        jdbcTemplate.execute("insert into catagory (id, name, is_enabled ) values ( 2, 'LUNCH', TRUE)");
+        jdbcTemplate.execute("insert into catagory (id, name, is_enabled ) values ( 3, 'BEVERAGES', TRUE)");
+        jdbcTemplate.execute("insert into catagory (id, name, is_enabled ) values ( 4, 'DESERTS', TRUE)");
+        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (1, 'CHILLY CHICKEN', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
+        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (2, 'TANDOORI CHICKEN', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
+        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (3, 'BUTTER CHICKEN', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
+        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (4, 'BEEF KABAB', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
+        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (5, 'LAMB BIRYANI', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
+        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (6, 'NAAN', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
+        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (7, 'LASSI', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
+        jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 1,1)");
+        jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 2,1)");
+        jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 3,1)");
+        jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 4,1)");
+        jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 5,1)");
+        jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 6,1)");
+        jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 7,2)");
+        jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 2,2)");
+        jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 6,2)");
+        jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 7,3)");
+        
+    }
 
     @Override
     protected void onTearDownAfterTransaction() throws Exception {
 
         super.onTearDownAfterTransaction();
-        deleteFromTables(new String[]{"company", "users"});
+        deleteFromTables(new String[]{"item_catagory", "item", "catagory"});
     }
 
-    public CatagoryServiceTest (String testName) {
-        super (testName);
+    public CatagoryServiceTest(String testName) {
+        super(testName);
     }
 
     public void testGetCatagories() {
         System.out.println("getCatagories");
-        CatagoryService instance = itemService;
-        List expResult = null;
-        List result = instance.getCatagories();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        
+        List result = catagoryService.getCatagories();
+        assertNotNull ( result );
+        assertTrue ( result.size() == 4 );
+        System.out.println ( result );
     } /* Test of getCatagories method, of class ItemService. */
 
     public void testGetCatagoryItems() {
         System.out.println("getCatagoryItems");
-        Long catagory = 0L;
-        CatagoryService instance = itemService;
-        List expResult = null;
-        List result = instance.getCatagoryItems(catagory);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        Long catagory = 1L;
+        
+        List result = catagoryService.getCatagoryItems(catagory);
+        assertNotNull ( result );
+        assertTrue ( result.size() == 6 );
+        System.out.println ( result );
     } /* Test of getCatagoryItems method, of class ItemService. */
-    
 }
