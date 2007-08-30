251fff2dcbdd9a369b06cc9a1dc2ed2e9f3e62c6
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-08-30 03:36:45 +0000

    updated

--- trunk/ECommerce-DAO/src/com/abbhsoft/ecommerce/daoImpl/ItemDAOImpl.java
+++ trunk/ECommerce-DAO/src/com/abbhsoft/ecommerce/daoImpl/ItemDAOImpl.java
@@ -26,8 +26,17 @@ public class ItemDAOImpl extends GenericAbstractDAO<Item, Long> implements ItemD
     public List<Item> findBySearchKeys(String... keys) {
         // construct a query
         // use preparedstatement like params
-        // construct a map of params [name, value]
-        // call this.findByJPAQL(sql, params) and return the List
+//        select * from item
+//         where (name like :key1 and name like :key2 and name like 1key3 ) or
+//        (description like :key1 and description like :key2 and description like :key3  ) or
+//        (ingredients like :key1 and ingredients like :key2 and ingredients like :key3  );
+        
+//         construct a map of params [name, value]
+//         Map<String, Object> map = ..
+//         map.put ( "Key1", keys[0]);
+//         map.put ( "Key2", keys[1]);
+//         map.put ( "Key3", keys[2]);
+        // call this.findByJPAQL(sql, map) and return the List
         return null;
     }
 
