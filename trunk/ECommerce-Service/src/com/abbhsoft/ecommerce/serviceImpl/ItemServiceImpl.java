772a602204c4762a270a1a97768a419a0b2159f1
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-08-30 03:18:32 +0000

    updated

--- trunk/ECommerce-Service/src/com/abbhsoft/ecommerce/serviceImpl/ItemServiceImpl.java
+++ trunk/ECommerce-Service/src/com/abbhsoft/ecommerce/serviceImpl/ItemServiceImpl.java
@@ -1,8 +1,8 @@
 /*
  * ItemImpl.java
- * 
+ *
  * Created on Aug 24, 2007, 11:52:09 PM
- * 
+ *
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -12,27 +12,39 @@ package com.abbhsoft.ecommerce.serviceImpl;
 import com.abbhsoft.ecommerce.dao.ItemDAO;
 import com.abbhsoft.ecommerce.model.Item;
 import com.abbhsoft.ecommerce.service.ItemService;
+import com.abbhsoft.sqlInjectionFilter.SQLInjectionFilterManager;
 import java.util.Collection;
+import java.util.List;
 
 
 /**
  *
  * @author Mohammed Hamed
  */
-public class ItemServiceImpl implements ItemService{
+public class ItemServiceImpl implements ItemService {
 
     public ItemServiceImpl() {
     }
 
     public Collection<Item> getAllItems() {
-          return itemDAO.findAll();
-        
+        return itemDAO.findAll();
     }
 
+    public List<Item> getSearchedItems(String key) {
+        // make sure the string is not empty else return empty List but not Null
+        // apply SQLInjectionFilter by calling 
+        // u can find this jar inside dao/lib
+        key = SQLInjectionFilterManager.getInstance().filter(key);
+        // use String[] keys = key.split(" ");
+        // call dao
+        // return the result
+        return null;
+    }
+    
     public void setItemDAO(ItemDAO itemDAO) {
         this.itemDAO = itemDAO;
     }
 
-
-private ItemDAO itemDAO;
+    
+    private ItemDAO itemDAO;
 }
