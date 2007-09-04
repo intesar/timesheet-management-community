02da249086c7f477b6bad44e325cc3af8805d100
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-09-04 20:14:16 +0000

    updated

--- trunk/ECommerce-Service/test/com/abbhsoft/ecommerce/service/ItemServiceTest.java
+++ trunk/ECommerce-Service/test/com/abbhsoft/ecommerce/service/ItemServiceTest.java
@@ -1,8 +1,8 @@
 /*
  * ItemServiceTest.java
- * 
+ *
  * Created on Aug 25, 2007, 6:50:25 AM
- * 
+ *
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -19,39 +19,36 @@ import java.util.Collection;
 import java.util.Iterator;
 import org.springframework.test.AbstractTransactionalDataSourceSpringContextTests;
 
-public class ItemServiceTest extends  AbstractTransactionalDataSourceSpringContextTests { 
+public class ItemServiceTest extends AbstractTransactionalDataSourceSpringContextTests {
 
-    
     private ItemService itemService;
+
     public ItemServiceTest() {
     }
 
-    
-    public void setItemService(ItemService itemService)
-    {
-        this.itemService=itemService;
+
+    public void setItemService(ItemService itemService) {
+        this.itemService = itemService;
     }
 
     @Override
     protected String[] getConfigLocations() {
-        return new String[]{"classpath:context/applicationContext-DAO.xml", 
-                             "classpath:context/applicationContext-Services.xml",
-                             "classpath:context/applicationContext-Transactions.xml"};
+        return new String[]{"classpath:context/applicationContext-DAO.xml", "classpath:context/applicationContext-Services.xml", "classpath:context/applicationContext-Transactions.xml"};
     }
-    
+
     @Override
     protected void onSetUpInTransaction() throws Exception {
- jdbcTemplate.execute("insert into catagory (id, name, is_enabled ) values ( 1, 'DINNER', TRUE)");
+        jdbcTemplate.execute("insert into catagory (id, name, is_enabled ) values ( 1, 'DINNER', TRUE)");
         jdbcTemplate.execute("insert into catagory (id, name, is_enabled ) values ( 2, 'LUNCH', TRUE)");
         jdbcTemplate.execute("insert into catagory (id, name, is_enabled ) values ( 3, 'BEVERAGES', TRUE)");
         jdbcTemplate.execute("insert into catagory (id, name, is_enabled ) values ( 4, 'DESERTS', TRUE)");
         jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (1, 'CHILLY CHICKEN', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
-        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (2, 'TANDOORI CHICKEN', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
-        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (3, 'BUTTER CHICKEN', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
-        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (4, 'BEEF KABAB', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
-        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (5, 'LAMB BIRYANI', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
-        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (6, 'NAAN', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
-        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (7, 'LASSI', 'CHIKEN WITH CHILLY GRAVY', 5.99, .75)");
+        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (2, 'TANDOORI CHICKEN', 'chicken grilled in a owen with chilly and spices', 5.99, .75)");
+        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (3, 'BUTTER CHICKEN', 'chicken cooked in chilly and butter gravy', 5.99, .75)");
+        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (4, 'BEEF KABAB', 'grilled beef baked on charkol', 5.99, .75)");
+        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (5, 'LAMB BIRYANI', 'traditional hyderabadi rice cooked with lamb', 5.99, .75)");
+        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (6, 'NAAN', 'baked flour', 5.99, .75)");
+        jdbcTemplate.execute("INSERT INTO ITEM ( ID, NAME, DESCRIPTION, PRICE, TAX ) VALUES (7, 'lassi', 'curd chilled with sugar and ice', 2.99, .75)");
         jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 1,1)");
         jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 2,1)");
         jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 3,1)");
@@ -62,11 +59,9 @@ public class ItemServiceTest extends  AbstractTransactionalDataSourceSpringConte
         jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 2,2)");
         jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 6,2)");
         jdbcTemplate.execute("INSERT INTO ITEM_CATAGORY ( ITEM_ID, CATAGORY_ID ) VALUES ( 7,3)");
-       
-        
     }
-    
-        @Override
+
+    @Override
     protected void onTearDownAfterTransaction() throws Exception {
 
         super.onTearDownAfterTransaction();
@@ -77,16 +72,45 @@ public class ItemServiceTest extends  AbstractTransactionalDataSourceSpringConte
         super(testName);
     }
 
-    public void testFindAll()
-    {
-        Collection<Item> items = itemService.getAllItems();   
+    public void testgetAll() {
+        Collection<Item> items = itemService.getAllItems();
         Iterator iter = items.iterator();
-     
-        
-            Item it = (Item)iter.next();
-            System.out.println(it.getName());
-        
-        }
-        
+        Item it = (Item) iter.next();
+        System.out.println(it.getName());
     }
-
+    
+    public void testgetMatchingItems1() {
+        String key = "LASSI";
+        Collection<Item> items = itemService.getMatchingItems(key);
+        assertTrue( items.size() == 1 );
+    }
+    public void testgetMatchingItems2() {
+        String key = "CURD SUGAR";
+        Collection<Item> items = itemService.getMatchingItems(key);
+        assertTrue( items.size() == 1 );
+    }
+    
+    public void testgetMatchingItems3() {
+        String key = "3";
+        Collection<Item> items = itemService.getMatchingItems(key);
+        assertTrue( items.size() == 1 );
+    }
+    
+    public void testgetMatchingItems4() {
+        String key = "3 9";
+        Collection<Item> items = itemService.getMatchingItems(key);
+        assertTrue( items.size() == 6 );
+    }
+    
+    public void testgetMatchingItems5() {
+        String key = "3..9";
+        Collection<Item> items = itemService.getMatchingItems(key);
+        assertTrue( items.size() == 6 );
+    }
+    public void testgetMatchingItems6() {
+        String key = "cHICKEN CHILLY";
+        Collection<Item> items = itemService.getMatchingItems(key);
+        assertTrue( items.size() == 3 );
+    }
+    
+}
