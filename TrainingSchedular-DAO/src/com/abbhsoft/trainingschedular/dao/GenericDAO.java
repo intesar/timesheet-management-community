11db89d6fa2c39ab85cef8ad89b55e43c5f5636e
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-10-26 20:01:38 +0000

    training dao project

---
+++ TrainingSchedular-DAO/src/com/abbhsoft/trainingschedular/dao/GenericDAO.java
@@ -0,0 +1,64 @@
+/*
+ * GenericDAO.java
+ * 
+ * Created on Aug 12, 2007, 4:03:00 PM
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.abbhsoft.trainingschedular.dao;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ *
+ * @param T 
+ * @param Pk 
+ * @author shannan
+ */
+public interface GenericDAO<T, Pk> {
+
+    /**
+     * 
+     * @param newInstance 
+     */
+    public abstract void save ( T newInstance);
+    
+    /**
+     * 
+     * @param pk 
+     * @return 
+     */
+    public abstract T findById ( Pk pk );
+    /**
+     * 
+     * @return 
+     */
+    public abstract Collection<T> findAll();
+    
+    /**
+     * 
+     * @param property 
+     * @param value 
+     * @return 
+     */
+    public abstract Collection<T> findByProperty(final String property, final Object value);
+    
+    /**
+     * 
+     * @param sql 
+     * @param params 
+     * @return 
+     */
+    public Collection<T> findBySQL(final String sql, final Map<String, Object> params);
+    
+    /**
+     * 
+     * @param ql 
+     * @param params 
+     * @return 
+     */
+    public Collection<T> findByJPAQL ( final String ql, final Map<String, Object> params );
+}
