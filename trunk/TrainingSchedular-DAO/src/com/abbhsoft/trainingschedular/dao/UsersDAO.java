412ec77b51aa7e3d6e53bb3a70a0a7e929c871ba
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-10-27 20:07:06 +0000

    dao and impl added

---
+++ trunk/TrainingSchedular-DAO/src/com/abbhsoft/trainingschedular/dao/UsersDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.abbhsoft.trainingschedular.dao;
+
+import com.abbhsoft.trainingschedular.model.Users;
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author mdshannan
+ */
+public interface UsersDAO extends GenericDAO<Users,BigDecimal>{
+
+}
