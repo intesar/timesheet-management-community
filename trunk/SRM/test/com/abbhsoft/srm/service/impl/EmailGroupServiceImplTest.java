5f86ccaece82accd82498a1f6f0b595bca55ef45
hossainsadd@gmail.com <hossainsadd@gmail.com@20ed3424-da33-0410-92c3-5597d9071429>
2007-11-17 20:19:52 +0000

    added the find methods needed extended from baseDao

--- trunk/SRM/test/com/abbhsoft/srm/service/impl/EmailGroupServiceImplTest.java
+++ trunk/SRM/test/com/abbhsoft/srm/service/impl/EmailGroupServiceImplTest.java
@@ -5,61 +5,53 @@
 
 package com.abbhsoft.srm.service.impl;
 
-import com.abbhsoft.srm.dao.EmailGroupDao;
+import com.abbhsoft.srm.base.BaseTest;
 import com.abbhsoft.srm.model.EmailGroup;
 import java.util.List;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.abbhsoft.srm.service.EmailGroupService;
 
 /**
  *
  * @author mdshannan
  */
-public class EmailGroupServiceImplTest extends TestCase {
+public class EmailGroupServiceImplTest extends BaseTest {
     
     public EmailGroupServiceImplTest(String testName) {
         super(testName);
     }
+    
+    private EmailGroupService emailGroupService;
+    
+    
 
-    public static Test suite() {
-        TestSuite suite = new TestSuite(EmailGroupServiceImplTest.class);
-        return suite;
+   public void setEmailGroupService(EmailGroupService emailGroupService) {
+        this.emailGroupService = emailGroupService;
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
 
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
+   
 
     /**
      * Test of recentEmailGroups method, of class EmailGroupServiceImpl.
      */
     public void testRecentEmailGroups() {
         System.out.println("recentEmailGroups");
-        EmailGroupServiceImpl instance = new EmailGroupServiceImpl();
-        List<EmailGroup> expResult = null;
-        List<EmailGroup> result = instance.recentEmailGroups();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        
+        EmailGroup email1=new EmailGroup(0L,"hossainsadd@gmail.com");
+        EmailGroup email2=new EmailGroup(1L,"bllllllllll");
+        EmailGroup email3=new EmailGroup(2L,"ddddddddddddd");
+        
+        emailGroupService.save(email1);
+        emailGroupService.save(email2);
+        emailGroupService.save(email3);
+        
+        List<EmailGroup> result = emailGroupService.recentEmailGroups();
+        assertTrue(result.size() >= 3);
 
-    /**
-     * Test of save method, of class EmailGroupServiceImpl.
-     */
-    public void testSave() {
-        System.out.println("save");
-        EmailGroup group = null;
-        EmailGroupServiceImpl instance = new EmailGroupServiceImpl();
-        instance.save(group);
+        
+        
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+       
     }
 
     
