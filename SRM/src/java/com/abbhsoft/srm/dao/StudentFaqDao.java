d0fc31fcd64319ded5137eebcdfdda0945bcfdbd
hossainsadd@gmail.com <hossainsadd@gmail.com@20ed3424-da33-0410-92c3-5597d9071429>
2007-11-17 04:01:25 +0000

    added the find methods needed extended from baseDao

---
+++ SRM/src/java/com/abbhsoft/srm/dao/StudentFaqDao.java
@@ -0,0 +1,24 @@
+/*
+ * StudentFaqDao.java
+ * 
+ * Created on Nov 17, 2007, 9:42:02 PM
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.abbhsoft.srm.dao;
+
+import com.abbhsoft.srm.base.GenericDao;
+import com.abbhsoft.srm.model.StudentFaq;
+import java.util.List;
+
+/**
+ *
+ * @author Sadd
+ */
+public interface StudentFaqDao extends GenericDao <StudentFaq, Long> {
+    public List<StudentFaq> findByConvincedRate(String convincedRate );
+    public List<StudentFaq> findByStudent(Long studentId );
+
+}
