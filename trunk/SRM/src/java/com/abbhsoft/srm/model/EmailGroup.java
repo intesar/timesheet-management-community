8b71bc002b142edb4e8b207cfba447d2182ba564
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-11-17 19:42:50 +0000

    refactored

--- trunk/SRM/src/java/com/abbhsoft/srm/model/EmailGroup.java
+++ trunk/SRM/src/java/com/abbhsoft/srm/model/EmailGroup.java
@@ -24,16 +24,16 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "email_group")
 @NamedQueries({
-    @NamedQuery(name = "Group1.findById", query = "SELECT g FROM Group1 g WHERE g.id = ?1"), 
-    @NamedQuery(name = "Group1.findByOwner", query = "SELECT g FROM Group1 g WHERE g.owner = ?1"),
-    @NamedQuery(name = "Group1.findBySubscribeEmail", query = "SELECT g FROM Group1 g WHERE g.subscribeEmail = ?1"), 
-    @NamedQuery(name = "Group1.findByGroupEmail", query = "SELECT g FROM Group1 g WHERE g.groupEmail = ?1"), 
-    @NamedQuery(name = "Group1.findByUnsubscribeEmail", query = "SELECT g FROM Group1 g WHERE g.unsubscribeEmail = ?1"),
-    @NamedQuery(name = "Group1.findByCreateUser", query = "SELECT g FROM Group1 g WHERE g.createUser = ?1"), 
-    @NamedQuery(name = "Group1.findByCreateDate", query = "SELECT g FROM Group1 g WHERE g.createDate = ?1"), 
-    @NamedQuery(name = "Group1.findByLastModifiedUser", query = "SELECT g FROM Group1 g WHERE g.lastModifiedUser = ?1"),
-    @NamedQuery(name = "Group1.findByLastModifiedDate", query = "SELECT g FROM Group1 g WHERE g.lastModifiedDate = ?1"),
-    @NamedQuery(name = "Group1.findAll", query = "SELECT g FROM Group1 g ")})
+    @NamedQuery(name = "EmailGroup.findById", query = "SELECT g FROM EmailGroup g WHERE g.id = ?1"), 
+    @NamedQuery(name = "EmailGroup.findByOwner", query = "SELECT g FROM EmailGroup g WHERE g.owner = ?1"),
+    @NamedQuery(name = "EmailGroup.findBySubscribeEmail", query = "SELECT g FROM EmailGroup g WHERE g.subscribeEmail = ?1"), 
+    @NamedQuery(name = "EmailGroup.findByGroupEmail", query = "SELECT g FROM EmailGroup g WHERE g.groupEmail = ?1"), 
+    @NamedQuery(name = "EmailGroup.findByUnsubscribeEmail", query = "SELECT g FROM EmailGroup g WHERE g.unsubscribeEmail = ?1"),
+    @NamedQuery(name = "EmailGroup.findByCreateUser", query = "SELECT g FROM EmailGroup g WHERE g.createUser = ?1"), 
+    @NamedQuery(name = "EmailGroup.findByCreateDate", query = "SELECT g FROM EmailGroup g WHERE g.createDate = ?1"), 
+    @NamedQuery(name = "EmailGroup.findByLastModifiedUser", query = "SELECT g FROM EmailGroup g WHERE g.lastModifiedUser = ?1"),
+    @NamedQuery(name = "EmailGroup.findByLastModifiedDate", query = "SELECT g FROM EmailGroup g WHERE g.lastModifiedDate = ?1"),
+    @NamedQuery(name = "EmailGroup.findAll", query = "SELECT g FROM EmailGroup g ")})
 public class EmailGroup extends BaseModel implements java.io.Serializable  {
     private static final long serialVersionUID = 1L;
    
@@ -45,12 +45,12 @@ public class EmailGroup extends BaseModel implements java.io.Serializable  {
     private String groupEmail;
     @Column(name = "unsubscribe_email")
     private String unsubscribeEmail;
-    @OneToMany(mappedBy = "group1")
-    private Collection<Student> studentCollection;
     @JoinColumn(name = "university", referencedColumnName = "id")
     @ManyToOne
     private University university;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "group_")
+    @OneToMany(mappedBy = "group1")
+    private Collection<Student> studentCollection;
+    @OneToMany(mappedBy = "group1")
     private Collection<Event> eventCollection;
 
     public Collection<Event> getEventCollection() {
