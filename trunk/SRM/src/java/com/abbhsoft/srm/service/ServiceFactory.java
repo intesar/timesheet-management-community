79d321a850cb95fbdb8cc894d7c7bd86d221ea76
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-11-17 07:04:53 +0000

    udpated

---
+++ trunk/SRM/src/java/com/abbhsoft/srm/service/ServiceFactory.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.abbhsoft.srm.service;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author mdshannan
+ */
+public class ServiceFactory {
+
+    BeanFactory factory = null;
+
+    private ServiceFactory() {
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+            new String[]{"classpath:context/applicationContext-Dao.xml",
+                "classpath:context/applicationContext-Dao-Extention.xml",
+                "classpath:context/applicationContext-Services.xml",
+                "classpath:context/applicationContext-Transactions.xml"});
+
+        // of course, an ApplicationContext is just a BeanFactory
+        factory = (BeanFactory) context;
+    }
+
+    // bean names : eventServiceImpl
+    public Object getBean (String bean ) {
+        return factory.getBean(bean);
+    }
+
+    public static final ServiceFactory instance = new ServiceFactory();
+
+    public static final ServiceFactory getInstance() {
+        return instance;
+    }
+}
