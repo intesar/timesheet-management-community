826275780b00915dda2712caa4d6492e26236e8e
hossainsadd@gmail.com <hossainsadd@gmail.com@20ed3424-da33-0410-92c3-5597d9071429>
2007-11-17 02:38:50 +0000

    deleted   id and other 4 things and their getter setters

--- SRM/src/java/com/abbhsoft/srm/model/Group1.java
+++ SRM/src/java/com/abbhsoft/srm/model/Group1.java
@@ -5,20 +5,16 @@
 
 package com.abbhsoft.srm.model;
 
-import java.io.Serializable;
+import com.abbhsoft.srm.base.BaseModel;
 import java.util.Collection;
-import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 /**
  *
@@ -27,11 +23,9 @@ import javax.persistence.TemporalType;
 @Entity
 @Table(name = "group")
 @NamedQueries({@NamedQuery(name = "Group1.findById", query = "SELECT g FROM Group1 g WHERE g.id = :id"), @NamedQuery(name = "Group1.findByOwner", query = "SELECT g FROM Group1 g WHERE g.owner = :owner"), @NamedQuery(name = "Group1.findBySubscribeEmail", query = "SELECT g FROM Group1 g WHERE g.subscribeEmail = :subscribeEmail"), @NamedQuery(name = "Group1.findByGroupEmail", query = "SELECT g FROM Group1 g WHERE g.groupEmail = :groupEmail"), @NamedQuery(name = "Group1.findByUnsubscribeEmail", query = "SELECT g FROM Group1 g WHERE g.unsubscribeEmail = :unsubscribeEmail"), @NamedQuery(name = "Group1.findByCreateUser", query = "SELECT g FROM Group1 g WHERE g.createUser = :createUser"), @NamedQuery(name = "Group1.findByCreateDate", query = "SELECT g FROM Group1 g WHERE g.createDate = :createDate"), @NamedQuery(name = "Group1.findByLastModifiedUser", query = "SELECT g FROM Group1 g WHERE g.lastModifiedUser = :lastModifiedUser"), @NamedQuery(name = "Group1.findByLastModifiedDate", query = "SELECT g FROM Group1 g WHERE g.lastModifiedDate = :lastModifiedDate")})
-public class Group1 implements Serializable {
+public class Group1 extends BaseModel {
     private static final long serialVersionUID = 1L;
-    @Id
-    @Column(name = "id", nullable = false)
-    private Integer id;
+   
     @Column(name = "owner")
     private String owner;
     @Column(name = "subscribe_email")
@@ -40,16 +34,6 @@ public class Group1 implements Serializable {
     private String groupEmail;
     @Column(name = "unsubscribe_email")
     private String unsubscribeEmail;
-    @Column(name = "create_user")
-    private Integer createUser;
-    @Column(name = "create_date")
-    @Temporal(TemporalType.DATE)
-    private Date createDate;
-    @Column(name = "last_modified_user")
-    private Integer lastModifiedUser;
-    @Column(name = "last_modified_date")
-    @Temporal(TemporalType.DATE)
-    private Date lastModifiedDate;
     @OneToMany(mappedBy = "group1")
     private Collection<Student> studentCollection;
     @JoinColumn(name = "university", referencedColumnName = "id")
@@ -59,22 +43,15 @@ public class Group1 implements Serializable {
     public Group1() {
     }
 
-    public Group1(Integer id) {
+    public Group1(Long id) {
         this.id = id;
     }
 
-    public Group1(Integer id, String groupEmail) {
+    public Group1(Long id, String groupEmail) {
         this.id = id;
         this.groupEmail = groupEmail;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
     public String getOwner() {
         return owner;
@@ -107,39 +84,6 @@ public class Group1 implements Serializable {
     public void setUnsubscribeEmail(String unsubscribeEmail) {
         this.unsubscribeEmail = unsubscribeEmail;
     }
-
-    public Integer getCreateUser() {
-        return createUser;
-    }
-
-    public void setCreateUser(Integer createUser) {
-        this.createUser = createUser;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public Integer getLastModifiedUser() {
-        return lastModifiedUser;
-    }
-
-    public void setLastModifiedUser(Integer lastModifiedUser) {
-        this.lastModifiedUser = lastModifiedUser;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
     public Collection<Student> getStudentCollection() {
         return studentCollection;
     }
