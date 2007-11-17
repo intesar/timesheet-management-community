42cfe81117e96e0fcdff352752df596b71947b37
hossainsadd@gmail.com <hossainsadd@gmail.com@20ed3424-da33-0410-92c3-5597d9071429>
2007-11-17 02:51:12 +0000

    deleted   id and other 4 things and their getter setters

--- trunk/SRM/src/java/com/abbhsoft/srm/model/Address.java
+++ trunk/SRM/src/java/com/abbhsoft/srm/model/Address.java
@@ -5,18 +5,15 @@
 
 package com.abbhsoft.srm.model;
 
+import com.abbhsoft.srm.base.BaseModel;
 import java.io.Serializable;
 import java.util.Collection;
-import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 /**
  *
@@ -25,11 +22,9 @@ import javax.persistence.TemporalType;
 @Entity
 @Table(name = "address")
 @NamedQueries({@NamedQuery(name = "Address.findById", query = "SELECT a FROM Address a WHERE a.id = :id"), @NamedQuery(name = "Address.findByType", query = "SELECT a FROM Address a WHERE a.type = :type"), @NamedQuery(name = "Address.findByStreet", query = "SELECT a FROM Address a WHERE a.street = :street"), @NamedQuery(name = "Address.findByCity", query = "SELECT a FROM Address a WHERE a.city = :city"), @NamedQuery(name = "Address.findByState", query = "SELECT a FROM Address a WHERE a.state = :state"), @NamedQuery(name = "Address.findByZipcode", query = "SELECT a FROM Address a WHERE a.zipcode = :zipcode"), @NamedQuery(name = "Address.findByCountry", query = "SELECT a FROM Address a WHERE a.country = :country"), @NamedQuery(name = "Address.findByCreateUser", query = "SELECT a FROM Address a WHERE a.createUser = :createUser"), @NamedQuery(name = "Address.findByCreateDate", query = "SELECT a FROM Address a WHERE a.createDate = :createDate"), @NamedQuery(name = "Address.findByLastModiiedUser", query = "SELECT a FROM Address a WHERE a.lastModiiedUser = :lastModiiedUser"), @NamedQuery(name = "Address.findByLastModifiedDate", query = "SELECT a FROM Address a WHERE a.lastModifiedDate = :lastModifiedDate")})
-public class Address implements Serializable {
+public class Address extends BaseModel implements Serializable {
     private static final long serialVersionUID = 1L;
-    @Id
-    @Column(name = "id", nullable = false)
-    private Integer id;
+    
     @Column(name = "type")
     private String type;
     @Column(name = "street", nullable = false)
@@ -42,38 +37,21 @@ public class Address implements Serializable {
     private String zipcode;
     @Column(name = "country")
     private String country;
-    @Column(name = "create_user")
-    private Integer createUser;
-    @Column(name = "create_date")
-    @Temporal(TemporalType.DATE)
-    private Date createDate;
-    @Column(name = "last_modiied_user")
-    private Integer lastModiiedUser;
-    @Column(name = "last_modified_date")
-    @Temporal(TemporalType.DATE)
-    private Date lastModifiedDate;
+   
     @OneToMany(mappedBy = "address")
     private Collection<Campus> campusCollection;
 
     public Address() {
     }
 
-    public Address(Integer id) {
+    public Address(Long id) {
         this.id = id;
     }
 
-    public Address(Integer id, String street, String city) {
+    public Address(Long id, String street, String city) {
         this.id = id;
         this.street = street;
         this.city = city;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public String getType() {
@@ -122,42 +100,6 @@ public class Address implements Serializable {
 
     public void setCountry(String country) {
         this.country = country;
-    }
-
-    public Integer getCreateUser() {
-        return createUser;
-    }
-
-    public void setCreateUser(Integer createUser) {
-        this.createUser = createUser;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public Integer getLastModiiedUser() {
-        return lastModiiedUser;
-    }
-
-    public void setLastModiiedUser(Integer lastModiiedUser) {
-        this.lastModiiedUser = lastModiiedUser;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public Collection<Campus> getCampusCollection() {
-        return campusCollection;
     }
 
     public void setCampusCollection(Collection<Campus> campusCollection) {
