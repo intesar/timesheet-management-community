d71ba98d52670cfb15bd8296f515881e97faa064
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-11-21 06:42:43 +0000

    updates

--- SRM/src/java/com/abbhsoft/srm/service/remote/AddEntityRemoteService.java
+++ SRM/src/java/com/abbhsoft/srm/service/remote/AddEntityRemoteService.java
@@ -4,12 +4,15 @@
  */
 package com.abbhsoft.srm.service.remote;
 
+import com.abbhsoft.srm.model.Address;
 import com.abbhsoft.srm.model.EmailGroup;
 import com.abbhsoft.srm.model.Event;
+import com.abbhsoft.srm.model.Student;
 import com.abbhsoft.srm.model.University;
 import com.abbhsoft.srm.service.EmailGroupService;
 import com.abbhsoft.srm.service.EventService;
 import com.abbhsoft.srm.service.ServiceFactory;
+import com.abbhsoft.srm.service.StudentService;
 import com.abbhsoft.srm.service.UniversityService;
 
 /**
@@ -18,6 +21,11 @@ import com.abbhsoft.srm.service.UniversityService;
  */
 public class AddEntityRemoteService {
 
+    /*
+     *  takes university name and adds to the database
+     *  throws runtime exception if university already exists
+     * 
+     */
     public String addUniversity(String name) {
         String msg = "Operation Successful!";
         UniversityService service = (UniversityService) ServiceFactory.getInstance().getBean("universityServiceImpl");
@@ -33,11 +41,16 @@ public class AddEntityRemoteService {
         }
     }
 
+    /*
+     *  creates a new event
+     *  date cannot be null
+     * 
+     */
     public String addEvent(String type, String description, String date, String priority) {
         String msg = "Operation Successful!";
         EventService service = (EventService) ServiceFactory.getInstance().getBean("eventServiceImpl");
         Event e = new Event();
-        if ( type == null || type.length() < 1 ) {
+        if (type == null || type.length() < 1) {
             return "Type is Required!";
         }
         e.setType(type);
@@ -58,7 +71,12 @@ public class AddEntityRemoteService {
             return msg;
         }
     }
-    
+
+    /*
+     *  creates a new group
+     *  groupEmail cannot be null
+     * 
+     */
     public String addEmailGroup(String emailGroup, String owner, String subscribe, String unsubscribe) {
         String msg = "Operation Successful!";
         EmailGroupService service = (EmailGroupService) ServiceFactory.getInstance().getBean("emailGroupServiceImpl");
@@ -71,6 +89,64 @@ public class AddEntityRemoteService {
             service.save(e);
         } catch (RuntimeException re) {
             msg = "Error Creating Email Group!";
+            throw re;
+        } finally {
+            return msg;
+        }
+    }
+
+    /*
+     *  creates a new student
+     * 
+     */
+    public String addStudent(String firstname, String mi, String lastname,
+            String email, String secondaryEmail, String mobile, String homePhone,
+            String fax, String otherPhone, String education, String visaStatus,
+            String gender, String age, String nationality, String refferedBy,
+            String expectedStartDate, String tag, String street, String city,
+            String state, String zipcode, String country) {
+        String msg = "Operation Successful!";
+        try {
+            Student s = new Student();
+            s.setFirstName(firstname);
+            s.setMi(mi);
+            s.setLastName(lastname);
+            s.setEmail(email);
+            s.setSecondaryEmail(secondaryEmail);
+            s.setMobilePhone(mobile);
+            s.setHomePhone(homePhone);
+            s.setFax(fax);
+            s.setOtherPhone(otherPhone);
+            s.setEducation(education);
+            s.setVisaStatus(visaStatus);
+            s.setGender(gender);
+            s.setAge(age);
+            s.setNationality(nationality);
+            s.setRefferedBy(refferedBy);
+            if (expectedStartDate != null && expectedStartDate.length() > 0) {
+                try {
+                    java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("MM/dd/yy");
+                    java.util.Date dt = sdf.parse(expectedStartDate);
+                    s.setExpectedStartDate(dt);
+                } catch (java.text.ParseException p) {
+                    msg += " (Warning : Invalid Expected Start Date)";
+                }
+            }
+            s.setTag(tag);
+            if ((city != null && city.length() > 0) ||
+                    (street != null && street.length() > 0)) {
+                Address a = new Address();
+                a.setStreet(street);
+                a.setCity(city);
+                a.setState(state);
+                a.setZipcode(zipcode);
+                a.setCountry(country);
+                s.setAddress(a);
+            }
+            StudentService service = (StudentService) ServiceFactory.getInstance().getBean("studentServiceImpl");
+            service.save(s);
+        } catch (RuntimeException re) {
+            msg = "Error Creating Student!";
             throw re;
         } finally {
             return msg;
