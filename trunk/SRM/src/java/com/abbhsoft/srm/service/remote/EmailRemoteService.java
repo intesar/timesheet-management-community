e8d9db910af09bc25151dfa8f524b75cca355c03
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2007-11-24 08:01:09 +0000

    updates

--- trunk/SRM/src/java/com/abbhsoft/srm/service/remote/EmailRemoteService.java
+++ trunk/SRM/src/java/com/abbhsoft/srm/service/remote/EmailRemoteService.java
@@ -5,10 +5,16 @@
 
 package com.abbhsoft.srm.service.remote;
 
+import com.abbhsoft.srm.service.EMailService;
+import com.abbhsoft.srm.service.ServiceFactory;
+
 /**
  *
  * @author mdshannan
  */
 public class EmailRemoteService {
-
+    public void sentEmailToAddressWithBody(String to, String body) {
+        EMailService service = (EMailService) ServiceFactory.getInstance().getBean("eMailServiceImpl");
+        
+    }
 }
