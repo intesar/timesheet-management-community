a15db42aeae1bacd80afd461d440125c0385535a
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2008-01-27 02:42:17 +0000

    UPDATE

--- trunk/SRM/src/java/com/abbhsoft/srm/service/EMailService.java
+++ trunk/SRM/src/java/com/abbhsoft/srm/service/EMailService.java
@@ -14,13 +14,15 @@ public interface EMailService {
 
     static final String SMTP_HOST_NAME = "smtp.gmail.com";
     static final String SMTP_PORT = "465";
-    static final String emailMsgTxt = "ABBHSOFT Reminder";
-    static final String emailSubjectTxt = "ABBHSOFT Reminder, Batching starting soon," +
+    static final String EMAIL_MESSAGE_TEXT = "ABBHSOFT Reminder  <input type='text' name='name' />";
+    static final String EMAIL_SUBJECT_TEXT = "ABBHSOFT Reminder, Batching starting soon," +
             "Contact Us ASAP...";
-    static final String emailFromAddress = "mdshannan@gmail.com";
+    static final String EMAIL_FROM_ADDRESS = "mdshannan@gmail.com";
     static final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
-    static final String[] sendTo = {"mohdshannan@yahoo.com"};
-
+    static final String[] SEND_TO = {"mohdshannan@yahoo.com"};
+    static final String SEND_FROM_USERNAME = "mdshannan1";
+    static final String SEND_FROM_PASSWORD = "Abbh1234";
+    static final String EMAIL_CONTENT_TYPE = "text/html";
     void SendMail(String[] sendTo);
     void sendEmail (String toAddress, String body );
     EmailTemplate getEmailTemplate(Long id);
