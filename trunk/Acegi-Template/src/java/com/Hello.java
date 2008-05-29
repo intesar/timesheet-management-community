709ce968b9c67c0121e14df1adf4883cb579e321
shannan.mohammed <shannan.mohammed@20ed3424-da33-0410-92c3-5597d9071429>
2008-05-29 19:00:44 +0000

    updated code

---
+++ trunk/Acegi-Template/src/java/com/Hello.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com;
+
+import java.util.Date;
+
+/**
+ *
+ * @author intesar
+ */
+public class Hello  {
+
+    public Hello () {
+        
+    }
+    public  void sayHello () {
+        Date dt = new Date();
+        System.out.println ( dt );
+    }
+
+    
+    
+}
