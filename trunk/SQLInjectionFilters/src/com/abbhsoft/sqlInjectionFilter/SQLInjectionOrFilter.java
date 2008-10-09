91d5cfdab18b61aba7c40d5137c8de4023141942
mdshannan <mdshannan@20ed3424-da33-0410-92c3-5597d9071429>
2008-10-09 20:26:58 +0000

    updated

--- trunk/SQLInjectionFilters/src/com/abbhsoft/sqlInjectionFilter/SQLInjectionOrFilter.java
+++ trunk/SQLInjectionFilters/src/com/abbhsoft/sqlInjectionFilter/SQLInjectionOrFilter.java
@@ -6,13 +6,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.abbhsoft.sqlInjectionFilter;
 
 /**
  *
  * @author shannan
- *  
+ *  finds patterns like (or, and, =, true, --, ;, insert, drop, alter, shutdown
  *  removes ( or, OR ) 
  */
 class SQLInjectionOrFilter implements SQLInjectionFilter {
@@ -21,9 +20,48 @@ class SQLInjectionOrFilter implements SQLInjectionFilter {
     }
 
     public String filter(String filter) {
-        filter = filter.replace("or", SPACE );
-        filter = filter.replace("OR", SPACE );
+        String str = filter;
+        int count = 0;
+        str = str.toLowerCase();
+        if (str.contains("or")) {
+            count++;
+        }
+        if (str.contains("and")) {
+            count++;
+        }
+        if (str.contains("true")) {
+            count++;
+        }
+        if (str.contains("insert")) {
+            count++;
+        }
+        if (str.contains("select")) {
+            count++;
+        }
+        if (str.contains("update")) {
+            count++;
+        }
+        if (str.contains("drop")) {
+            count++;
+        }
+        if (str.contains("alter")) {
+            count++;
+        }
+        if (str.contains("shutdown")) {
+            count++;
+        }
+        if (count > 1) {
+            str = str.replace("or", SPACE);
+            str = str.replace("and", SPACE);
+            str = str.replace("true", SPACE);
+            str = str.replace("insert", SPACE);
+            str = str.replace("select", SPACE);
+            str = str.replace("update", SPACE);
+            str = str.replace("drop", SPACE);
+            str = str.replace("alter", SPACE);
+            str = str.replace("shutdown", SPACE);
+        }
+
         return filter;
     }
-
 }
